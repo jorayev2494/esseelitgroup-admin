8001f4cd715ae32addd7eda20fed44826efb6125
Yakub Jorayev <jorayev2494@gmail.com>
2024-02-26 03:40:03 +0400

    Fixing dev mode

--- src/services/http/index.js
+++ src/services/http/index.js
@@ -6,13 +6,26 @@ const getBackendAddress = () => {
   const addresses = {
     local: 'http://127.0.0.1:8088',
     develop: 'http://212.24.101.35:8088',
+    development: 'http://212.24.101.35:8088',
     production: 'http://127.0.0.1:8088',
   }
 
   return addresses[process.env.NODE_ENV] ?? addresses['local']
 }
 
-const backendAddress = getBackendAddress();
+console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
+
+const addresses = {
+  local: 'http://127.0.0.1:8088',
+  develop: 'http://212.24.101.35:8088',
+  development: 'http://212.24.101.35:8088',
+  production: 'http://127.0.0.1:8088',
+}
+
+// return addresses[process.env.NODE_ENV] ?? addresses['local']
+
+// const backendAddress = getBackendAddress();
+const backendAddress = addresses[process.env.NODE_ENV] ?? addresses['local'];
 const baseURL = `${backendAddress}/api/admin`;
 
 const httpClient = axios.create({
