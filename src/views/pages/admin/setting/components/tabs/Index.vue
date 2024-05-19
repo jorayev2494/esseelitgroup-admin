33196ca7412bfaa8be1a46d460dd007c42cac93f
Yakub Jorayev <jorayev2494@gmail.com>
2024-05-20 02:35:39 +0400

    added About Us

---
+++ src/views/pages/admin/setting/components/tabs/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="settings-menu-links">
+    <ul class="nav nav-tabs menu-tabs">
+
+      <li
+        class="nav-item"
+        v-for="({ label, route }, idx) of items" :key="idx"
+        :class="currentPath == route.name ? 'active' : 'notactive'"
+      >
+        <router-link class="nav-link" :to="$tMakeRoute(route)">
+          {{ $t(label) }}
+        </router-link>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script setup>
+  import useBehavior from "./useBehavior";
+
+  const {
+    currentPath,
+    items,
+  } = useBehavior()
+
+</script>
---
+++ src/views/pages/admin/setting/pages/index/Index.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <Tabs></Tabs>
+  <router-view></router-view>
+
+</template>
+
+<script setup>
+  import Tabs from '../../components/tabs/Index.vue'
+
+</script>
