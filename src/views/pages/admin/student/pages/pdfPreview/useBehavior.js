3afc9fd01e151c3a3f685e15a9bc12ad0d253c78
Yakub Jorayev <jorayev2494@gmail.com>
2024-06-03 19:46:33 +0400

    Student generate pdf

---
+++ src/views/pages/admin/student/pages/pdfPreview/useBehavior.js
@@ -0,0 +1,31 @@
+import httpClient from "@/services/http";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import { useStore } from "vuex"
+
+export default () => {
+  const route = useRoute();
+  const store = useStore();
+
+  const { uuid } = route.params;
+
+  const src = ref(null);
+  const token = ref(null);
+
+  const loadPdfPreview = () => {
+    token.value ??= store.getters['auth/getAccessToken'];
+
+    src.value = httpClient.getUri();
+    src.value = src.value.substring(0, src.value.indexOf('/api'))
+
+    src.value += `/admin/students/pdf/preview/${uuid}?token=${token.value}`
+  }
+
+  onMounted(() => {
+    loadPdfPreview()
+  })
+
+  return {
+    src,
+  }
+}
